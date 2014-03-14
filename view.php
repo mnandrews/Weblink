dabc5040565a5310813e379cbd4501ce29341705
Mark Andrews <mnandrews@me.com>
2014-03-14 19:05:07 +0000

    renaming vars in view.php

--- view.php
+++ view.php
@@ -16,58 +16,58 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * URL module main user interface
+ * weblink module main user interface
  *
  * @package    mod
- * @subpackage url
- * @copyright  2009 Petr Skoda  {@link http://skodak.org}
+ * @subpackage weblink
+ * @copyright  
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 require('../../config.php');
-require_once("$CFG->dirroot/mod/url/locallib.php");
+require_once("$CFG->dirroot/mod/weblink/locallib.php");
 require_once($CFG->libdir . '/completionlib.php');
 
 $id       = optional_param('id', 0, PARAM_INT);        // Course module ID
-$u        = optional_param('u', 0, PARAM_INT);         // URL instance id
+$u        = optional_param('u', 0, PARAM_INT);         // weblink instance id
 $redirect = optional_param('redirect', 0, PARAM_BOOL);
 
 if ($u) {  // Two ways to specify the module
-    $url = $DB->get_record('url', array('id'=>$u), '*', MUST_EXIST);
-    $cm = get_coursemodule_from_instance('url', $url->id, $url->course, false, MUST_EXIST);
+    $weblink = $DB->get_record('weblink', array('id'=>$u), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_instance('weblink', $weblink->id, $weblink->course, false, MUST_EXIST);
 
 } else {
-    $cm = get_coursemodule_from_id('url', $id, 0, false, MUST_EXIST);
-    $url = $DB->get_record('url', array('id'=>$cm->instance), '*', MUST_EXIST);
+    $cm = get_coursemodule_from_id('weblink', $id, 0, false, MUST_EXIST);
+    $weblink = $DB->get_record('weblink', array('id'=>$cm->instance), '*', MUST_EXIST);
 }
 
 $course = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);
 
 require_course_login($course, true, $cm);
 $context = context_module::instance($cm->id);
-require_capability('mod/url:view', $context);
+require_capability('mod/weblink:view', $context);
 
-add_to_log($course->id, 'url', 'view', 'view.php?id='.$cm->id, $url->id, $cm->id);
+add_to_log($course->id, 'weblink', 'view', 'view.php?id='.$cm->id, $weblink->id, $cm->id);
 
 // Update 'viewed' state if required by completion system
 $completion = new completion_info($course);
 $completion->set_module_viewed($cm);
 
-$PAGE->set_url('/mod/url/view.php', array('id' => $cm->id));
+$PAGE->set_weblink('/mod/weblink/view.php', array('id' => $cm->id));
 
-// Make sure URL exists before generating output - some older sites may contain empty urls
-// Do not use PARAM_URL here, it is too strict and does not support general URIs!
-$exturl = trim($url->externalurl);
-if (empty($exturl) or $exturl === 'http://') {
-    url_print_header($url, $cm, $course);
-    url_print_heading($url, $cm, $course);
-    url_print_intro($url, $cm, $course);
-    notice(get_string('invalidstoredurl', 'url'), new moodle_url('/course/view.php', array('id'=>$cm->course)));
+// Make sure weblink exists before generating output - some older sites may contain empty urls
+// Do not use PARAM_weblink here, it is too strict and does not support general URIs!
+$extweblink = trim($weblink->externalweblink);
+if (empty($extweblink) or $extweblink === 'http://') {
+    weblink_print_header($weblink, $cm, $course);
+    weblink_print_heading($weblink, $cm, $course);
+    weblink_print_intro($weblink, $cm, $course);
+    notice(get_string('invalidstoredweblink', 'weblink'), new moodle_weblink('/course/view.php', array('id'=>$cm->course)));
     die;
 }
-unset($exturl);
+unset($extweblink);
 
-$displaytype = url_get_final_display_type($url);
+$displaytype = weblink_get_final_display_type($weblink);
 if ($displaytype == RESOURCELIB_DISPLAY_OPEN) {
     // For 'open' links, we always redirect to the content - except if the user
     // just chose 'save and display' from the form then that would be confusing
@@ -77,37 +77,37 @@ if ($displaytype == RESOURCELIB_DISPLAY_OPEN) {
 }
 
 if ($redirect) {
-    // coming from course page or url index page,
+    // coming from course page or weblink index page,
     // the redirection is needed for completion tracking and logging
-    $fullurl = str_replace('&amp;', '&', url_get_full_url($url, $cm, $course));
+    $fullweblink = str_replace('&amp;', '&', weblink_get_full_weblink($weblink, $cm, $course));
 
     if (!course_get_format($course)->has_view_page()) {
         // If course format does not have a view page, add redirection delay with a link to the edit page.
-        // Otherwise teacher is redirected to the external URL without any possibility to edit activity or course settings.
-        $editurl = null;
+        // Otherwise teacher is redirected to the external weblink without any possibility to edit activity or course settings.
+        $editweblink = null;
         if (has_capability('moodle/course:manageactivities', $context)) {
-            $editurl = new moodle_url('/course/modedit.php', array('update' => $cm->id));
+            $editweblink = new moodle_weblink('/course/modedit.php', array('update' => $cm->id));
             $edittext = get_string('editthisactivity');
         } else if (has_capability('moodle/course:update', $context->get_course_context())) {
-            $editurl = new moodle_url('/course/edit.php', array('id' => $course->id));
+            $editweblink = new moodle_weblink('/course/edit.php', array('id' => $course->id));
             $edittext = get_string('editcoursesettings');
         }
-        if ($editurl) {
-            redirect($fullurl, html_writer::link($editurl, $edittext)."<br/>".
+        if ($editweblink) {
+            redirect($fullweblink, html_writer::link($editweblink, $edittext)."<br/>".
                     get_string('pageshouldredirect'), 10);
         }
     }
-    redirect($fullurl);
+    redirect($fullweblink);
 }
 
 switch ($displaytype) {
     case RESOURCELIB_DISPLAY_EMBED:
-        url_display_embed($url, $cm, $course);
+        weblink_display_embed($weblink, $cm, $course);
         break;
     case RESOURCELIB_DISPLAY_FRAME:
-        url_display_frame($url, $cm, $course);
+        weblink_display_frame($weblink, $cm, $course);
         break;
     default:
-        url_print_workaround($url, $cm, $course);
+        weblink_print_workaround($weblink, $cm, $course);
         break;
 }
